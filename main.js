53db743e7dbacb76816a0a02794082ff7ba5f093
hakylepremier <62679468+hakylepremier@users.noreply.github.com>
2022-12-03 15:35:24 +0000

    Add website files

---
+++ main.js
@@ -0,0 +1,8 @@
+// let element = document.createElement('article');
+// element.innerText = "Click Me";
+// element.className = "popup";
+
+// document.querySelectorAll(".services").forEach(item => {item.addEventListener("mouseover", function () {
+//     item.appendChild(element);
+// } ) } )
+
