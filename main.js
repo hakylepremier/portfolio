a63fd3ee6d8b3bdc90465605817e82c51837422f
Haky <hakylepremier@gmail.com>
2022-12-09 12:39:54 +0000

    Add toggle buttons on phone

--- main.js
+++ main.js
@@ -6,3 +6,23 @@
 //     item.appendChild(element);
 // } ) } )
 
+const navToggle = document.querySelector('.toggle-button');
+const searchToggle = document.querySelector('.search-button');
+const navList = document.querySelector('.nav-list');
+const searchBar = document.querySelector('.search-bar');
+
+
+const toggleBtns = [navToggle, searchToggle]
+
+toggleBtns.forEach((e)=>{
+    e.addEventListener('click', ()=>{
+        if (e === navToggle) {
+            navList.classList.toggle('nav-active');
+            searchBar.classList.remove('search-active');
+        }else if (e === searchToggle) {
+            searchBar.classList.toggle('search-active');
+            navList.classList.remove('nav-active');
+        }
+    })
+})
+// navToggle.addEventListener('click', )
